8d2fefc6ef8b9f4c6af675bdb8a7b711f1328ab2
ClaireL21 <53claire@gmail.com>
2024-09-06 11:23:20 -0400

    basic carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,192 @@
 import { useState } from 'react'
 import { ChakraProvider, useToast } from "@chakra-ui/react";
-import { Heading } from "@chakra-ui/react"
+import { Heading, Image } from "@chakra-ui/react"
+import { Tag, TagLabel, HStack, FormControl, Flex } from '@chakra-ui/react'
 import { Text, Container, VStack, Box, SimpleGrid, Button } from '@chakra-ui/react'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+//  const [count, setCount] = useState(0)
 
   return (
     <ChakraProvider isResettingCSS={false} >
+      <Box
+        backgroundImage="url('/images/background_sky.png')"
+        backgroundSize="cover"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat">
+      
+        <VStack w="full" h="100vh">
+          <Heading mt='50px' fontSize='xl' fontWeight='normal' color='white' fontFamily='Playfair Display, sans-serif'>
+            PHILADELPHIA
+          </Heading>
+          <Text fontWeight='hairline' fontSize='8xl' color='white' mt='-8'>
+            61°
+          </Text>
+          <Text fontWeight='normal' fontSize='xl' color='white' mt='-8'>
+            L: 58° H: 64°
+          </Text>
+
+          <Container mt='10px' as='section' maxWidth='4xl' justify='center'>
+            <Heading color='white' fontWeight='normal' fontSize='xl' fontFamily='Playfair Display, sans-serif' width='full'>
+              10-Day Forecast
+            </Heading>
+          </Container>
+          <VStack>
+            <Box px={4} width='400px' height='200px'> {/* Add padding here */}
+              <HStack margin='2px' spacing={1} overflowX="auto">
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Today</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/sun_icon.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Sunny 60°</Text>
+                  </CardBody>
+                </Card>
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Fri 06</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/sun_icon.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Sunny 65°</Text>
+                  </CardBody>
+                </Card>
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Sat 07</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/white_cloud_img.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Cloudy 62°</Text>
+                  </CardBody>
+                </Card>
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Sun 08</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/sun_icon.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Sunny 64°</Text>
+                  </CardBody>
+                </Card>
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Mon 09</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/sun_icon.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Sunny 60°</Text>
+                  </CardBody>
+                </Card>
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Tues 10</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/white_cloud_img.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Cloudy 54°</Text>
+                  </CardBody>
+                </Card>
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Wed 11</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/white_cloud_img.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Cloudy 54°</Text>
+                  </CardBody>
+                </Card>
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Thu 12</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/white_cloud_img.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Cloudy 60°</Text>
+                  </CardBody>
+                </Card>
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Fri 13</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/sun_icon.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Sunny 60°</Text>
+                  </CardBody>
+                </Card>
+                <Card margin='2px' width='122px' height='140px' flexShrink='0' backgroundColor='whiteAlpha.800'>
+                  <CardHeader>
+                    <Heading size='md'>Sat 14</Heading>
+                  </CardHeader>
+                  <CardBody mt='-30px'>
+                    <Flex justifyContent='center'>
+                    <Image src='/images/sun_icon.png' width='50px'></Image>
+                    </Flex>
+                    <Text>Sunny 60°</Text>
+                  </CardBody>
+                </Card>
+              </HStack>
+            </Box>
+          </VStack>
+
+{/* 
+          <Box px={4}>
+            <VStack>
+              <HStack spacing={4} overflowX="auto">
+                <Card margin='8px' padding='4' flexShrink='0'>
+                  <CardHeader>
+                    <Heading size='md'> Customer dashboard</Heading>
+                  </CardHeader>
+                  <CardBody>
+                    <Text>View a summary of all your customers over the last month.</Text>
+                  </CardBody>
+                  <CardFooter>
+                    <Button>View here</Button>
+                  </CardFooter>
+                </Card>
+                <Card margin='8' padding='4' flexShrink='0'>
+                  <CardHeader>
+                    <Heading size='md'> Customer dashboard</Heading>
+                  </CardHeader>
+                  <CardBody>
+                    <Text>View a summary of all your customers over the last month.</Text>
+                  </CardBody>
+                  <CardFooter>
+                    <Button>View here</Button>
+                  </CardFooter>
+                </Card>
+              </HStack>
+            </VStack>
+          </Box> */}
+
+          
+        </VStack>
+      </Box>
+      
+      {/* <Flex justify='center'>
+        <Heading my='5px' p='10px'>Chakra UI Components</Heading>
+      </Flex>
       <Box
         height="100vh"
         backgroundImage="url('/images/background_sky.png')"
@@ -23,11 +200,93 @@ function App() {
           <Heading fontSize='xl' fontWeight='normal' color='white' fontFamily='Playfair Display, sans-serif'>PHILADELPHIA</Heading>
           <Text fontWeight='hairline' fontSize='8xl' color='white' mt='-8'>61°</Text>
           <Text fontWeight='normal' fontSize='xl' color='white' mt='-8'>L: 58° H: 64°</Text>
+
+          <VStack align="flex-start">
+            <HStack spacing={4} overflowX="auto">
+              <Tag
+                flexShrink="0"
+                size="md"
+                key={0}
+                borderRadius="full"
+                variant="solid"
+                colorScheme={"green"}
+              >
+                <TagLabel>HI THERE</TagLabel>
+              </Tag>
+              <Tag
+                flexShrink="0"
+                size="md"
+                key={1}
+                borderRadius="full"
+                variant="solid"
+                colorScheme={"green"}
+              >
+                <TagLabel>HI THERE</TagLabel>
+              </Tag>
+              <Tag
+                flexShrink="0"
+                size="md"
+                key={2}
+                borderRadius="full"
+                variant="solid"
+                colorScheme={"green"}
+              >
+                <TagLabel>HI THERE</TagLabel>
+              </Tag>
+              <Tag
+                flexShrink="0"
+                size="md"
+                key={3}
+                borderRadius="full"
+                variant="solid"
+                colorScheme={"green"}
+              >
+                <TagLabel>HI THERE</TagLabel>
+              </Tag>
+              <Tag
+                flexShrink="0"
+                size="md"
+                key={4}
+                borderRadius="full"
+                variant="solid"
+                colorScheme={"green"}
+              >
+                <TagLabel>HI</TagLabel>
+              </Tag>
+            </HStack>
+          </VStack>
+
+          <VStack align="flex-start">
+            <HStack spacing={4} overflowX="auto">
+              <Card margin='8' padding='4' flexShrink='0'>
+                <CardHeader>
+                  <Heading size='md'> Customer dashboard</Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>View a summary of all your customers over the last month.</Text>
+                </CardBody>
+                <CardFooter>
+                  <Button>View here</Button>
+                </CardFooter>
+              </Card>
+              <Card margin='8' padding='4' flexShrink='0'>
+                <CardHeader>
+                  <Heading size='md'> Customer dashboard</Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text>View a summary of all your customers over the last month.</Text>
+                </CardBody>
+                <CardFooter>
+                  <Button>View here</Button>
+                </CardFooter>
+              </Card>
+            </HStack>
+          </VStack>
           
-          <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(100px, 1fr))'>
+          <SimpleGrid overflow='scroll' spacing={4} templateColumns='repeat(auto-fill, minmax(100px, 1fr))'>
             <Card>
               <CardHeader>
-                <Heading size='sm'> Customer dashboard</Heading>
+                <Heading size='md'> Customer dashboard</Heading>
               </CardHeader>
               <CardBody>
                 <Text>View a summary of all your customers over the last month.</Text>
@@ -38,7 +297,7 @@ function App() {
             </Card>
             <Card>
               <CardHeader>
-                <Heading size='sm'> Customer dashboard</Heading>
+                <Heading size='md'> Customer dashboard</Heading>
               </CardHeader>
               <CardBody>
                 <Text>View a summary of all your customers over the last month.</Text>
@@ -49,7 +308,7 @@ function App() {
             </Card>
             <Card>
               <CardHeader>
-                <Heading size='sm'> Customer dashboard</Heading>
+                <Heading size='md'> Customer dashboard</Heading>
               </CardHeader>
               <CardBody>
                 <Text>View a summary of all your customers over the last month.</Text>
@@ -84,9 +343,9 @@ function App() {
             <Text>View a summary of all your customers over the last month.</Text>
           </CardBody>
         </Card>
-      </Box>
+      </Box> */}
     </ChakraProvider>
-  )
+  );
 }
 
 export default App

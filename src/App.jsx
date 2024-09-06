4e38092c4c7eb69f38345a5ae6f861ea9fd967e6
ClaireL21 <53claire@gmail.com>
2024-09-05 22:51:26 -0400

    added background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { ChakraProvider, useToast } from "@chakra-ui/react";
-import { Text } from '@chakra-ui/react'
+import { Heading } from "@chakra-ui/react"
+import { Text, Container, VStack, Box } from '@chakra-ui/react'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import './App.css'
 
@@ -9,12 +10,45 @@ function App() {
 
   return (
     <ChakraProvider isResettingCSS={false} >
-    <Card>
-      <CardBody>
-        <Text>View a summary of all your customers over the last month.</Text>
-      </CardBody>
-    </Card>
-  </ChakraProvider>
+      <Box
+        height="100vh"
+        backgroundImage="url('/images/background_sky.png')"
+        backgroundSize="cover"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+      >
+        <Text color="white" fontSize="2xl" p={4}>
+          Welcome to the App
+        </Text>
+        <VStack mt='24px'>
+          <Heading>Philadelphia</Heading>
+          <Text fontSize='8xl' padding='1' mt='-10'>61°</Text>
+          <Container maxW='md' bg='blue.600' color='white' centerContent>
+            "md" Container
+          </Container>
+          <Container maxW='550px' bg='purple.600' color='white'>
+            "550px" Container
+          </Container>
+          <Container maxW='container.sm' bg='green.400' color='#262626'>
+            "container.sm" Container
+          </Container>
+
+          <Container maxW='2xl' bg='blue.600' centerContent>
+            <Box padding='4' bg='white' color='black' maxW='md'>
+              There are many benefits to a joint design and development system. Not only
+              does it bring benefits to the design team, but it also brings benefits to
+              engineering teams. It makes sure that our experiences have a consistent look
+              and feel, not just in our design specs, but in production.
+            </Box>
+          </Container>
+        </VStack>
+        <Card>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+        </Card>
+      </Box>
+    </ChakraProvider>
   )
 }
 
